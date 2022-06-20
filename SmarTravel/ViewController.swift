9e956a8c6c774412a360bb3a36bdaa710abef37e
Roman <roma321m@gmail.com>
2022-06-20 11:05:35 +0200

    Initial Commit

---
+++ SmarTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmarTravel
+//
+//  Created by user216779 on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
